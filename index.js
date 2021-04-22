679b690bf17ff1d70fafd8fede916e415b3ac9d0
Kiana Long <kianajade42.kl@gmail.com>
2021-04-22 15:06:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class Bird {
   }
   speak(){
     if (this.sex==`male`){
-    return `It's me! ${this.name}`
+    return `It's me! ${this.name}, the parrot!`
   }
   else {
       return `${this.name} says squawk!`
